ec9ca9ef7059e93ea15d728708e8532d7c448ef6
tuomaskokeileekoodia <tuomaslahti89@gmail.com>
2019-08-20 16:43:54 +0300

    Paiva 2. Tehtävien listaus toteutettu MainComponentin avulla, this.state otettu käyttöön, Footer tuotu mukaan

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-
-
+import { View, Text, StyleSheet } from 'react-native';
+import NavBar from "../components/NavBar";
+import MainComponent from "../components/MainComponent";
+import Footer from "../components/Footer";
 
 const HomeScreen = () => {
-    return <Text style={styles.text}>Anna tän toimia</Text>;
+    return (
+        <View>
+            <NavBar/>
+            {/*<Text style={styles.saatanallinenTeksti}>Saatana</Text>*/}
+            <MainComponent/>
+            {/*<Text style={styles.saatanallinenTeksti}>Ystävä</Text>*/}
+            <Footer/>
+        </View>
+    );
 };
- const styles = StyleSheet.create({
-     text:{
-         fontSize: 30
-     }
- });
-
-
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 30
+    },
+    saatanallinenTeksti:{
+        fontSize: 50,
+        color: 'red'
+    }
+});
 export default HomeScreen;
