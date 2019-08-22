83b1632dfe16e6597c9ccc392523b4b1d59c0d7d
LauriKukko <lauri.kukkonen@gmail.com>
2019-08-22 09:18:33 +0300

    Header on muokattu

--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -10,12 +10,14 @@ import AddTaskScreen from "./AddTaskScreen";
 const Navigation = createStackNavigator(
     {
         Login: LoginScreen,
-        Home: HomeScreen ,//tähän perään saa valumaan propseja. Selvitä.
+        Home: HomeScreen,//tähän perään saa valumaan propseja. Selvitä.
         Menu: MenuScreen,
         AddTask: AddTaskScreen,
     },
     {
         initialRouteName: 'Login',
+        //headerMode hävittää täältä tuon backbuttonin, mutta tekee sen nyt kaikilla sivuilla ja ei estä fyysisen backbuttonin painamista.
+        headerMode: "none"
         /*  defaultNavigationOptions:{
             title: 'Sun Mutsis',
               headerLeft: <Button onPress={() => props.navigation.navigate('tähän halutun screenin nimi')} title= "HOME?BACK?" />,
