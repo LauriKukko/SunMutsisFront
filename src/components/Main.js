e638fa8650af05d88447d771014c5841890c500c
tuomaskokeileekoodia <tuomaslahti89@gmail.com>
2019-08-22 11:38:12 +0300

    Merge conflictia pukkaa

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View, TextInput, ScrollView, TouchableOpacity, StyleSheet} from 'react-native';
-
+import { CheckBox } from 'react-native-elements'
 
 // Main- ja Note -luokat korvaavat aiemman MainComponent -luokan (Tuomas)
 // Note.js:ssä propseina näytettävien todo:n päivämäärä, "nimi" sekä delete-näppäin (Tuomas)
@@ -12,10 +12,10 @@ export default class Main extends React.Component {
         super(props);
         this.state = {
             noteArray: [
-                {date: '2019/08/22', note: 'Tämä on todo YKSI',},
-                {date: '2019/08/22', note: 'Tämä on todo KAKSI'},
-                {date: '2019/08/22', note: 'Tämä on todo KOLME'},
-                {date: '2019/08/22', note: 'Tämä on todo NELJÄ'},
+                {date: '2019/8/22', note: 'Tämä on todo YKSI',},
+                {date: '2019/8/22', note: 'Tämä on todo KAKSI'},
+                {date: '2019/8/22', note: 'Tämä on todo KOLME'},
+                {date: '2019/8/22', note: 'Tämä on todo NELJÄ'},
             ],
             noteText: '',
         }
@@ -31,6 +31,42 @@ export default class Main extends React.Component {
 
         return (
             <View>
+                {/*<CheckBox*/}
+                {/*    title='Click Here'*/}
+                {/*    checked={this.state.checked}*/}
+                {/*/>*/}
+
+                {/*<CheckBox*/}
+                {/*    center*/}
+                {/*    title='Click Here'*/}
+                {/*    checked={this.state.checked}*/}
+                {/*/>*/}
+
+                {/*<CheckBox*/}
+                {/*    center*/}
+                {/*    title='Click Here'*/}
+                {/*    checkedIcon='dot-circle-o'*/}
+                {/*    uncheckedIcon='circle-o'*/}
+                {/*    checked={this.state.checked}*/}
+                {/*/>*/}
+
+                {/*<CheckBox*/}
+                {/*    center*/}
+                {/*    title='Click Here to Remove This Item'*/}
+                {/*    iconRight*/}
+                {/*    iconType='material'*/}
+                {/*    checkedIcon='clear'*/}
+                {/*    uncheckedIcon='add'*/}
+                {/*    checkedColor='red'*/}
+                {/*    checked={this.state.checked}*/}
+                {/*/>*/}
+
+                {/*<CheckBox*/}
+                {/*    checkedIcon={<Image source={require('../checked.png')} />}*/}
+                {/*    uncheckedIcon={<Image source={require('../unchecked.png')} />}*/}
+                {/*    checked={this.state.checked}*/}
+                {/*    onPress={() => this.setState({checked: !this.state.checked})}*/}
+                {/*/>*/}
                 <ScrollView style={styles.scrollContainer}>
                     {notes}
                 </ScrollView>
