a7cc6f7941495dbd96895e40bfd8e7fb0cadbabc
LauriKukko <lauri.kukkonen@gmail.com>
2019-08-22 17:06:23 +0300

    Christianin powerjumppa tietokannan tuomiseksi frontiin.

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,8 +9,8 @@ export default class Note extends React.Component {
 
             <View key={this.props.keyval} style={styles.note}>
 
-                <Text style={styles.noteText}>{this.props.val.date}</Text>
-                <Text style={styles.noteText}>{this.props.val.note}</Text>
+                <Text style={styles.noteText}>{JSON.stringify(this.props.val._id)}</Text>
+                <Text style={styles.noteText}>{JSON.stringify(this.props.val.task)}</Text>
 
                 <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
                     <Text style={styles.noteDeleteText}>X</Text>
