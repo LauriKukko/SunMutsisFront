279ddd4bbfa3b0ad31771b8625bdf7a8a5754013
saoltupp <saoltupp@gmail.com>
2019-08-23 16:47:00 +0300

    Muokattu footeria, etta flouttaa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,34 @@
 import React, {Component}  from 'react';
-import {Text, StyleSheet, View, TouchableHighlight, Button} from 'react-native';
-
+import {Text, StyleSheet, View, TouchableHighlight, Button, TouchableOpacity} from 'react-native';
 
 export default class Footer extends Component{
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View>
-                <TouchableHighlight style={styles.addButton} underlayColor='#ff7043' onPress={()=>navigate('AddTask')}>
+                <TouchableOpacity style={styles.addButton} underlayColor='#ff7043' onPress={()=>navigate('AddTask')}>
                     <Text style={{fontSize: 50, color: 'white'}}>+</Text>
-                </TouchableHighlight>
+                </TouchableOpacity>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-        addButton: {
+    addButton: {
         backgroundColor: '#56e4ff',
         borderColor: '#53d8ff',
         borderWidth: 1,
-        height: 50,
-        width: 50,
-        borderRadius: 50,
+        height: 60,
+        width: 60,
+        borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
-        bottom: -140,
+        bottom: 40,
         // left: 0,
         right: 10,
-        zIndex: 11,
+        zIndex: 20,
         // top: 480,
         // left: 290,
         shadowColor: "#000000",
